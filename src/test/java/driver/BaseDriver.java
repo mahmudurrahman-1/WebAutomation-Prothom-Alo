4271b03d401191da354352921d67e9cd6f97dffc
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-07-30 16:28:34 +0600

    BaseDriver inherited

--- src/test/java/driver/BaseDriver.java
+++ src/test/java/driver/BaseDriver.java
@@ -1,4 +1,5 @@
 package driver;
+
 import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -13,38 +14,36 @@ import java.time.Duration;
 
 public class BaseDriver {
     WebDriver driver = null;
-    public String baseurl="https://www.prothomalo.com/";
+    public String baseurl = "https://www.prothomalo.com/";
 
-    public void setup(){
-           String browser = System.getProperty("browser","chrome");
-           if(browser.contains("chrome")){
-               WebDriverManager.chromedriver().setup();
-               driver=new ChromeDriver();
-           }
-           else if(browser.contains("opera")){
-               WebDriverManager.operadriver().setup();
-               driver=new OperaDriver();
-           }
-           else if(browser.contains("firefox")){
-               WebDriverManager.firefoxdriver().setup();
-               driver=new FirefoxDriver();
-           }
-           else {
-               WebDriverManager.edgedriver().setup();
-               driver=new EdgeDriver();
-           }
-           driver.manage().window().maximize();
-           driver.get(baseurl);
+    public void setup() {
+        String browser = System.getProperty("browser", "chrome");
+        if (browser.contains("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.contains("opera")) {
+            WebDriverManager.operadriver().setup();
+            driver = new OperaDriver();
+        } else if (browser.contains("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        }
+        driver.manage().window().maximize();
+        driver.get(baseurl);
 
            /*implicity wait tells browser to wait for a certain amount of time before throwing
         n"No Such Elements Found"
         * */
-           driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-           PageDriver.getinstance().setdriver(driver);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        PageDriver.getinstance().setdriver(driver);
     }
+
     @AfterSuite
     @Step("Everthing will be quit after all process")
-    public void teardown(){
+    public void teardown() {
         PageDriver.getCurrentDriver().quit();
     }
 }
