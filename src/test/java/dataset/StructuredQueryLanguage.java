5af00bc18381f47d3814acbf815adcda804a791b
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-07-30 20:38:19 +0600

    search data

--- src/test/java/dataset/StructuredQueryLanguage.java
+++ src/test/java/dataset/StructuredQueryLanguage.java
@@ -13,4 +13,16 @@ public class StructuredQueryLanguage {
         Object[][] data = {{"Mahmudur Rahman", "mahmudur.rahman.seebpmmpo@gmail.com", "014125678Mr", "014125678Mr"}};
         return data;
     }
+    @Step("Login")
+    @DataProvider(name = "login")
+    public static Object[][] login() {
+        Object[][] data = {{"mahmudur.rahman.seebpmmpo@gmail.com", "014125678Mr"}};
+        return data;
+    }
+    @Step("Search")
+    @DataProvider(name = "searchdata")
+    public static Object[][] search() {
+        Object[][] data = {{"জম্মু"},{"পদ্মাসেতু"},{"লক্ষীপুর"},{"রামগঞ্জ"},{"ভোলা"},{"বরিশাল"},{"pakistan"},{"afganistan"},{"bangladesh"},{"Islam"},{"india"},{"al saudia"}};
+        return data;
+    }
 }
