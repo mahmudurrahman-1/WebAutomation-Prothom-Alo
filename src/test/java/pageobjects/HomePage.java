2f8b1b08872472f9a5cce9b83e65309c6324e2ea
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-07-30 20:37:55 +0600

    home

--- src/test/java/pageobjects/HomePage.java
+++ src/test/java/pageobjects/HomePage.java
@@ -1,6 +1,7 @@
 package pageobjects;
 
 import driver.PageDriver;
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -17,13 +18,55 @@ public class HomePage extends Common {
     //login button
     @FindBy(className = "button-m__type4__3WG12")
     public WebElement loginbutton;
+    @FindBy(className = "hamburger-m__hamburger__2Oc5s")
+    public WebElement menubutton;
+    @FindBy(className = "verticalMenubar-m__navbar-container__2TSE0")
+    public WebElement scrollContainer;
 
+    @FindBy(xpath = "//a[contains(text(),'ধর্ম')]")
+    public WebElement religion;
+    @FindBy(className = "stories2AdWithLoadMore-m__wrapper__2Nvc4")
+    public WebElement insideRel;
+
+    @FindBy(xpath = "//a[contains(text(),'বায়তুল মোকাররমে ঈদের প্রথম জামাত অনুষ্ঠিত')]")
+    public WebElement Secondlink;
+    public String textTwo="বায়তুল মোকাররমে ঈদের প্রথম জামাত অনুষ্ঠিত";
+
+    /***** Search Module locators ******/
+    @FindBy(className = "search-icon-wrapper")
+    public WebElement searchButton;
+    @FindBy(id="searchForm")
+    public WebElement searchInput;
+    @FindBy(className = "navbarSearch-m__search__form-submit__1OukE")
+    public WebElement searchSubmit;
+    /*******
+     * Actions
+     */
+
+    @Step("Search by texts")
+    public void searchthingsbytexts(String text){
+        searchButton.click();
+        sendText(searchInput,text);
+    }
+    public void searchSubmit(){
+       searchSubmit.click();
+    }
     /*******
      * Actions
      */
 
     public void Login(){
         loginbutton.click();
+    }
+    @Step("Checking menubar")
+    public void Menubar(){
+        menubutton.click();
+        scrollToElement(religion);
+        religion.click();
+    }
+    @Step("scroll down inside religion")
+    public void ThirdContainer(){
+        scrollToElement(insideRel);
     }
 
 }
